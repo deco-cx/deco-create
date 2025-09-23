70db5385e5fd5dd7555a85bc2f13b636e6a502e7
viktormarinho <viktormpcs@gmail.com>
2025-09-23 18:43:58 -0300

    wip

--- server/db.ts
+++ server/db.ts
@@ -3,10 +3,10 @@ import {
   migrateWithoutTransaction,
 } from "@deco/workers-runtime/drizzle";
 import type { Env } from "./main";
-import migrations from "./drizzle/migrations";
+// import migrations from "../drizzle/migrations";
 
 export const getDb = async (env: Env) => {
   const db = drizzle(env);
-  await migrateWithoutTransaction(db, migrations);
+  // await migrateWithoutTransaction(db, migrations);
   return db;
 };
